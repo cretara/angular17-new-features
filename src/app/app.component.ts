f6dfe6b2fa3461e1b8bfd54b7a34a969fb137788
Pier Daniele Cretara <Pier Daniele Cretara>
2023-11-23 12:42:18 +0100

    Added first example with checkbox Ng-Zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {Title} from "@angular/platform-browser";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {NzCheckboxModule} from "ng-zorro-antd/checkbox";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NgOptimizedImage],
+  imports: [CommonModule, RouterOutlet, NgOptimizedImage, FormsModule, NzCheckboxModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular17-new-features';
+
+  titleService = inject(Title);
+
+  public isChecked = signal(false);
+
+  public checkBoxFormControl: FormControl = new FormControl<boolean>(false)
+
+  constructor() {
+    this.titleService.setTitle(this.title);
+    effect(() => {
+      this.checkBoxFormControl.patchValue(this.isChecked());
+    })
+  }
+
 }
